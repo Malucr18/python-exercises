74eb8e37350a5f9c0f1d634b2878c72d464ebda3
Malucr18 <82298012+Malucr18@users.noreply.github.com>
2021-04-15 22:14:59 -0500

    style(linting)

--- app/menu.py
+++ app/menu.py
@@ -4,20 +4,21 @@ canva = Tk()
 canva.geometry('400x200')
 canva.title('Aplicación con Menú')
 
-#Se genera el menú
+# Se genera el menú
 menu_bar = Menu(canva)
-#Se inserta el menú
+# Se inserta el menú
 canva.config(menu=menu_bar)
 
-#Se crean las opciones a menú
+# Se crean las opciones a menú
 menu_opciones = Menu(menu_bar, tearoff=0)
 menu_opciones.add_command(label='Opción 1', command=lambda: frame_opcion1())
 menu_opciones.add_command(label='Opción 2', command=lambda: frame_opcion2())
 menu_opciones.add_separator()
 menu_opciones.add_command(label='Salir', command=canva.quit)
 
-#Se le agregan las opciones
+# Se le agregan las opciones
 menu_bar.add_cascade(label='Menú de Opciones', menu=menu_opciones)
+
 
 def frame_opcion1():
     frame_limpiar()
@@ -31,6 +32,7 @@ def frame_opcion1():
     label_1 = Label(frame_op1, text='Usuario').grid(row=0, column=0, padx=5, pady=5, sticky=NSEW)
     label_2 = Label(frame_op1, text='Contraseñ').grid(row=1, column=0, padx=5, pady=5, sticky=NSEW)
 
+
 def frame_opcion2():
     frame_limpiar()
     frame_op2 = Frame()
@@ -39,6 +41,7 @@ def frame_opcion2():
     frame_op2.config(relief="sunken")
     frame_op2.pack(fill="both", expand=1)
     frame_op2.columnconfigure(1, weight=1)
+
 
 def frame_limpiar():
     list = layout.pack_slaves()
